c32c10cec8d973cc58890f3bd7f19ff04ae74982
3aby3ee <s6806021412089@email.kmutnb.ac.th>
2025-08-09 22:33:35 +0700

    .

--- Rawiwan/lab6/lab6_005.cpp
+++ Rawiwan/lab6/lab6_005.cpp
@@ -3,39 +3,55 @@
 #include <string>
 
 using namespace std;
-int calgrade(int score);
+
+float calgrade(int score); 
 
 int main()
 {
     string Name;
-    int score = 0, grade;
-    float roomgpa = 0, roomavg = 0;
+    int score = 0;
+    float grade;
+    float roomgpa = 0;
+    float roomavg = 0;
     int Num;
+
+    cout << "Enter number of students: ";
+    cin >> Num;
+
     cout << fixed << setprecision(1);
+
     for (int i = 1; i <= Num; i++)
     {
         cout << "*********************************************************" << endl;
-        cout << "Enter Student[" << i << "] Name : " << endl;
+        cout << "Enter Student[" << i << "] Name : ";
         cin >> Name;
-        cout << "Enter Score: " << endl;
+        cout << "Enter Score: ";
         cin >> score;
+
         grade = calgrade(score);
-        cout << left << setw(16) << "Nopphagaw.t" << "Score = "
-             << right << setw(3) << 90
-             << "Grade =" << 4.0 << endl;
+        roomgpa += grade;
+
+        cout << left << setw(16) << Name << "Score = "
+             << right << setw(3) << score
+             << "   Grade = " << grade << endl;
     }
+
+    roomavg = roomgpa / Num;
+
     cout << "*********************************************************" << endl;
-    cout << "ROOM GRADE :" << roomgpa << endl;
+    cout << "ROOM GRADE : " << roomgpa << endl;
     cout << fixed << setprecision(2);
     cout << "AVERAGE OF GRADE = " << roomavg << endl;
     cout << "*********************************************************" << endl;
     cout << "------------------------ End Report ---------------------" << endl;
     cout << "*********************************************************" << endl;
+
     return 0;
 }
-int calgrade(int score)
+
+float calgrade(int score) 
 {
-   if (score >= 80) return 4.0;
+    if (score >= 80) return 4.0;
     else if (score >= 75) return 3.5;
     else if (score >= 70) return 3.0;
     else if (score >= 65) return 2.5;
@@ -43,5 +59,4 @@ int calgrade(int score)
     else if (score >= 55) return 1.5;
     else if (score >= 50) return 1.0;
     else return 0.0;
-    return score;
 }
