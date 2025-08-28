80f59290552403caef3aee506f908b575eac8322
3aby3ee <s6806021412089@email.kmutnb.ac.th>
2025-08-28 16:04:23 +0700

    Update lab7_2.cpp

--- Rawiwan/Lab7/lab7_2.cpp
+++ Rawiwan/Lab7/lab7_2.cpp
@@ -1,50 +1,16 @@
 #include <iostream>
 using namespace std;
+int Num1;
+int Num2;
 int main(){
-    int long A[5];
-    int B[5] = {3,4,5,6,7};
-    char C[] = {'1','2',65,44,'3'};
-    char D[] = "Hello";
-    int arr_size = 0;
-
-    cout << sizeof(A) << endl;
-    cout << sizeof(long long) << endl;
-/*
-    arr_size = sizeof(A)/sizeof(long long);
-    cout << arr_size << endl;
-    for(int i = 0; i < 5; i++){
-        A[i] = i*10;
-    }
-
-    /*for(int i =0; i < arr_size ; i++){
-        A[i] = 0;
-    }*/
-    D[4] = 'a';   
-    int index = 0;
-    for(char data: D){
-        cout << index <<":"<< data << endl;
-        index++;
-    }
-
-
-
-   /* A[8] = 130;
-    A[9] = 88;
-    for(int i =0; i < 10 ; i++){
-        cout << A[i] << " ";
-    };
+    cout << "Enter First Number :" ;
+    cin >> Num1;
+    cout << "\n Enter Second Number :" ;
+    cin >> Num2;
+    cout << "Total Sum : " << Num1 + Num2 << endl;
+    cout << "Total Neg : " << Num1 - Num2 << endl;
+    cout << "Total : " << Num1 * Num2 << endl;
+    cout << "Total : " << Num1 / Num2 << endl;
+    return 0;
     
-    cout << endl;*/
-
-
-    int* pointer = B;
-    cout << pointer << " " << *pointer << endl;
-    cout << B[0] << endl;
-
-    pointer =&B[1];
-    cout << pointer << " " << *pointer << endl;
-    cout << B[1] << endl;
-
-    
-    return (0);
 }
